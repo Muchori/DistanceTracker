272e07798a2d110f0717ddb806584faf997c5a0a
Muchori Joseph <36577265+Muchori@users.noreply.github.com>
2022-04-26 01:19:16 +0300

    Added Foreground service (#1)
    Added my location, Timer and services

--- app/src/main/java/com/muchori/joseph/distancetracker/ui/result/ResultFragment.kt
+++ app/src/main/java/com/muchori/joseph/distancetracker/ui/result/ResultFragment.kt
@@ -1,10 +1,11 @@
-package com.muchori.joseph.distancetracker
+package com.muchori.joseph.distancetracker.ui.result
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.muchori.joseph.distancetracker.R
 
 class ResultFragment : Fragment() {
 
