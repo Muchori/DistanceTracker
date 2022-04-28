a8ba6d204bb1265a3c2cf0e43009305a2270fbc2
Muchori Joseph <muchorijoseph97@gmail.com>
2022-04-28 20:07:15 +0300

    Added and Designed Result fragment, showing result of distance travelled and time taken

--- app/src/main/java/com/muchori/joseph/distancetracker/ui/result/ResultFragment.kt
+++ app/src/main/java/com/muchori/joseph/distancetracker/ui/result/ResultFragment.kt
@@ -4,17 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.muchori.joseph.distancetracker.R
+import com.muchori.joseph.distancetracker.databinding.FragmentResultBinding
 
-class ResultFragment : Fragment() {
+class ResultFragment : BottomSheetDialogFragment() {
+
+  private var _binding: FragmentResultBinding? = null
+  private val binding get() = _binding!!
+
+  private val args by navArgs<ResultFragmentArgs>()
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
-  ): View? {
+  ): View {
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_result, container, false)
+    _binding = FragmentResultBinding.inflate(inflater, container, false)
+
+    binding.distanceValueTextView.text = getString(R.string.result, args.result.distance)
+    binding.timeValueTextView.text = args.result.distance
+
+    return binding.root
   }
 
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+  }
 }
